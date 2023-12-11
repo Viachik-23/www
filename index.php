041829dcb38d155a80d9806c226ee889c6f14d02
MSI <kozachuk.vi@dvfu.ru>
2023-12-11 16:59:14 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/stile.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Новости</title>
+</head>
+<body>
+  <header>
+    <div id="logo">
+      <a href="/" title="Перейти на главную"><span>Н</span>овости</a>
+    </div>
+    <div id="menuHead">
+      <a href="/about.php"><div style="margin-right: 5%">О нас</div></a>
+      <a href="/feedback.php"><div>Обратная связь</div></a>
+    </div>
+    <div id="regAuth">
+      <a href="/reg.php">Регистрация</a> | <a href="auth.php">Авторизация</a>
+    </div>
+  </header>
+    </div>
+</body>
+</html>
