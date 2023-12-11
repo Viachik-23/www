5c9facdb5ab812bc9b3a0e2c3f13a5813ed49138
MSI <kozachuk.vi@dvfu.ru>
2023-12-11 17:05:52 +1000

    Сделал footer

--- index.php
+++ index.php
@@ -21,6 +21,27 @@
       <a href="/reg.php">Регистрация</a> | <a href="auth.php">Авторизация</a>
     </div>
   </header>
+
+  
+  <footer>
+    <div id="social">
+
+    <a href="https://vk.com/tv5priarg?ysclid=lppdz1bgfw274632583" title="Группа ВК" target="_blank">
+      <img src="img/vk.png" alt="ВК" title="ВК">
+    </a>
+
+    <a href="https://ok.ru/tv5priargunsk?ysclid=lppe30v0u8686358737" title="Группа OK" target="_blank">
+      <img src="img/ok.png" alt="OK" title="Одноклассники">
+    </a>
+
+    <a href="https://www.youtube.com/@VladPerminov" title="Канал" target="_blank">
+      <img src="img/youtube.png" alt="YouTube" title="YouTube">
+    </a>
+    </div>
+
+    <div id="rights">Все права защишены &copy <?=date ('Y')?></div>
+
+  </footer>
     </div>
 </body>
 </html>
