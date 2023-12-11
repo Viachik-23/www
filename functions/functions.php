23fdc68e3adad1cdcf556d03472e97a90d51e3ef
MSI <kozachuk.vi@dvfu.ru>
2023-12-11 18:13:51 +1000

    Создал базу данных со статьями и синхронизировал её с сайтом

--- functions/functions.php
+++ functions/functions.php
@@ -3,11 +3,26 @@
     function connectDB () {
         global $mysqli;
         $mysqli = new mysqli("localhost", "root", "", "newsbase");
-        $mysqli -> query("SET NAMES 'utf-8'");
+        $mysqli -> query("SET NAMES 'utf8'");
     }
 
     function closeDB () {
         global $mysqli;
         $mysqli -> close ();
+    }
+
+    function getNews($limit){
+        global $mysqli;
+        connectDB();
+        $result = $mysqli->query("SELECT * FROM `news` ORDER BY `id` DESC LIMIT $limit");
+        closeDB();
+        return resultToArray($result);
+    }
+
+    function resultToArray($result) {
+        $array = array ();
+        while(($row = $result->fetch_assoc()) != false)
+            $array[] = $row;
+        return $array;
     }
 ?>
